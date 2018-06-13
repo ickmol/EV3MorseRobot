d96fafce8dea589b1872631abfd83339d7d2016b
ickmol <ickmol@me.com>
2018-06-13 16:34:40 +1000

    InputComplete

--- BigMcThankiesFromMcSpankies.py
+++ BigMcThankiesFromMcSpankies.py
@@ -3,4 +3,4 @@ import ev3dev.ev3 as ev3
 ts = ev3.TouchSensor()
 while true:
     m = ev3.MediumMotor('outB')
-
+    
--- Morse.py
+++ Morse.py
@@ -4,41 +4,18 @@ import ev3dev.ev3 as ev3
 #Vars
 
 
+#Actuation
+Input = []
+
+TS1 = ev3.TouchSensor('in2')
+if True
+    input.append(".")
+
+TS2 = ev3.TouchSensor('in3')
+if True
+    input.append("-")
+
 #Translation
 
 
 #Display
-if a
-{
-
-}
-
-if b
-{
-
-}
-
-if c
-{
-
-}
-
-if d
-{
-
-}
-
-if e
-{
-
-}
-
-if f
-{
-
-}
-
-if g
-{
-
-}
